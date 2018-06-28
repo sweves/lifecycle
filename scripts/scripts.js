ac05625f3336529626638a59b956d9c4cf14b882
Sarah Wever <sarahewever@gmail.com>
2018-06-28 00:13:53 -0400

    clean up

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,4 @@
-
-
-
-
 window.onload = function() {
-
   // Video
   var video = document.getElementById("video");
 
@@ -27,45 +22,36 @@ window.onload = function() {
 
   video.play();
 
-  $('body').keyup(function(e){
-
-   if(e.keyCode == 32){
-       // user has pressed space
-       video.pause();
-   }
-   if(e.keyCode == 8){
-       // user has pressed backspace
-       video.play();
-   }
-
-});
+  $("body").keyup(function(e) {
+    if (e.keyCode == 32) {
+      // user has pressed space
+      video.pause();
+    }
+    if (e.keyCode == 8) {
+      // user has pressed backspace
+      video.play();
+    }
+  });
 
   //Event listener for the seek bar
   seekBar.addEventListener("change", function() {
-
     console.log(seekBar.value);
 
     //controlling video with 3 steps
 
-    if(seekBar.value == 0){
-
-      if (state == 1){
-
+    if (seekBar.value == 0) {
+      if (state == 1) {
         reversevid.currentTime = 14;
         reversevid.play();
 
         //play backwards from middle to beginning
         video.currentTime = loop1start;
         video.play();
-      
-        $('#reversevideo').removeClass('hidden');
-      
-        
+
+        $("#reversevideo").removeClass("hidden");
+
         console.log("backward from middle, state now 0");
         state = 0;
-
-
-
       } else if (state == 2) {
         //play backwards from end to beginning
 
@@ -76,42 +62,33 @@ window.onload = function() {
 
         console.log("backwards from end, state now 0");
         state = 0;
-
       }
-
     }
 
-    if(seekBar.value == 1){
-
-
-      if (state == 0){
+    if (seekBar.value == 1) {
+      if (state == 0) {
         //play from beginning to middle
         //start of transition
-        video.currentTime = (loop1end + 0.5);
+        video.currentTime = loop1end + 0.5;
         video.play();
 
         console.log("forward, state now 1");
         state = 1;
-
       } else if (state == 2) {
         //play from end to middle
         video.currentTime = loop2start;
         video.play();
         reversevid.currentTime = 0;
         reversevid.play();
-        $('#reversevideo').removeClass('hidden');
-        
+        $("#reversevideo").removeClass("hidden");
 
         console.log("backwards, state now 1");
         state = 1;
       }
-
     }
 
-    if(seekBar.value == 2){
-      
-
-      if (state == 0){
+    if (seekBar.value == 2) {
+      if (state == 0) {
         //play from beginning to end
 
         //TEMP
@@ -125,58 +102,48 @@ window.onload = function() {
         //play from middle to end
 
         //start of transition
-        video.currentTime = (loop2end + 0.5);
+        video.currentTime = loop2end + 0.5;
         video.play();
 
         console.log("forwards from middle, state now 2");
         state = 2;
       }
     }
-
   });
 
   // Update the seek bar as the video plays
   video.addEventListener("timeupdate", function() {
-
-
     //loop the first section
-    if(video.currentTime > loop1end && video.currentTime < (loop1end + 0.5)){
+    if (video.currentTime > loop1end && video.currentTime < loop1end + 0.5) {
       //start of loop
       video.currentTime = loop1start;
       video.play();
     }
-    
+
     //loop the middle section
-    if(video.currentTime > loop2end && video.currentTime < (loop2end + 0.5)){
+    if (video.currentTime > loop2end && video.currentTime < loop2end + 0.5) {
       //start of loop
       video.currentTime = loop2start;
       video.play();
     }
 
     //loop the end
-    if(video.currentTime > loop3end && video.currentTime < (loop3end + 0.5)){
+    if (video.currentTime > loop3end && video.currentTime < loop3end + 0.5) {
       //start of loop
       video.currentTime = loop3start;
       video.play();
     }
 
-    if(reversevid.currentTime > 4.7 && reversevid.currentTime < 5.3){
-      $('#reversevideo').addClass('hidden');
-      
+    if (reversevid.currentTime > 4.7 && reversevid.currentTime < 5.3) {
+      $("#reversevideo").addClass("hidden");
+
       // reversevid.pause();
     }
 
-    if(reversevid.currentTime > 18.5 && reversevid.currentTime < 19){
-      $('#reversevideo').addClass('hidden');
+    if (reversevid.currentTime > 18.5 && reversevid.currentTime < 19) {
+      $("#reversevideo").addClass("hidden");
       reversevid.currentTime = 0;
       reversevid.pause();
     }
-
-
-
   });
-
-
-}
-
-
+};
